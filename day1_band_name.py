cc597fcee41e4f78882968516651aae0d247a383
Shivam Sharma <ss5030200@gmail.com>
2026-03-16 23:31:26 +0530

    initial comm 1

---
+++ day1_band_name.py
@@ -0,0 +1,5 @@
+# first program of the 100 day camp - 
+print (" So you came for a band name eyee !!! ")
+city = input("tell me the first 3 letters of the city you were born in ")
+school = input("the school where your most years went in - only first 3 letters ")
+print(f"your band name is {city} {school}")
