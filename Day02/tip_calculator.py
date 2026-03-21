22e2271b32284e875ad52274dd34caf43815d36f
Shivam Sharma <ss5030200@gmail.com>
2026-03-22 02:33:45 +0530

    intification 2

--- Day02/tip_calculator.py
+++ Day02/tip_calculator.py
@@ -3,16 +3,16 @@ print("Welcome to the Tip Calculator")
 
 # Tip calculator program 
 def tip_calc (bill, tip_percent):
-    tip_tot = int(bill) * int(tip_percent)*0.01
-    return (int(bill) + tip_tot)
+    tip_tot = bill * tip_percent * 0.01
+    return bill + tip_tot
 
 # variable collection for calculation 
-bill_total = input("What is your total bill? $ ")
-tip = input("How much tip would you like to give % - 10, 12 or 15 ")
-total_BT = tip_calc(bill_total,tip)
+bill_total = int(input("What is your total bill? $ "))
+tip = int(input("How much tip would you like to give % - 10, 12 or 15 "))
+total_BT = int(tip_calc(bill_total,tip))
 
 # splitting bill logic 
-peeps = input ("how may people you wanna split the bill with? ")
+peeps = int(input ("how may people you wanna split the bill with? "))
 bill_per_persion = total_BT/peeps
 print(f"Each persion should pay : {bill_per_persion}")
 
