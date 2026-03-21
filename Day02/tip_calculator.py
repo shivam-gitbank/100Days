3d318bc098d874565c5bc923e11c71292c00839c
Shivam Sharma <ss5030200@gmail.com>
2026-03-22 02:39:08 +0530

    finalization

--- Day02/tip_calculator.py
+++ Day02/tip_calculator.py
@@ -7,12 +7,12 @@ def tip_calc (bill, tip_percent):
     return bill + tip_tot
 
 # variable collection for calculation 
-bill_total = int(input("What is your total bill? $"))
-tip = int(input("How much tip would you like to give 10, 12 or 15% "))
+bill_total = int(input("What is your total bill? $ "))
+tip = int(input("How much tip would you like to give 10, 12 or 15% = "))
 total_BT = int(tip_calc(bill_total,tip))
 
 # splitting bill logic 
-peeps = int(input ("how may people you wanna split the bill with? "))
+peeps = int(input ("how may people you wanna split the bill with? -- "))
 bill_per_persion = total_BT/peeps
-print(f"Each persion should pay : ${bill_per_persion}")
+print(f"Each persion should pay : $ {bill_per_persion}")
 
