3b436f4cd065a0ee2ae4251a9958174b5c7b3182
Shivam Sharma <ss5030200@gmail.com>
2026-03-23 22:50:01 +0530

    final tweaks

--- Day02/practice_day02.py
+++ Day02/practice_day02.py
@@ -1,4 +1,13 @@
 print("Numbers of letters in your name " + str(len(input("Enter your name "))))
 x = len(input("Enter: ")) * 2 + 3
 print(x)
-print((3 * ((3 + 3) / 3 )) - 3 )
+print((3 * ((3 + 3) / 3 )) - 3 )    
+
+height = 1.8
+wight = 74.3
+
+def calc(h1, w1):
+    return w1/(h1**2)
+
+bmi = calc(height, wight)
+print(round(bmi))
