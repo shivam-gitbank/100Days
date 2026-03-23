b2c50299ddef939f915476365099c135682c5e84
Shivam Sharma <ss5030200@gmail.com>
2026-03-23 22:51:40 +0530

    final tweaks

--- Day02/practice_day02.py
+++ Day02/practice_day02.py
@@ -1,8 +1,14 @@
 print("Numbers of letters in your name " + str(len(input("Enter your name "))))
+
+# PEMDAS example and toutorial - the PEMDAS rule moves from left to right --> 
 x = len(input("Enter: ")) * 2 + 3
 print(x)
+
+#pemdas to return output 3 
 print((3 * ((3 + 3) / 3 )) - 3 )    
 
+
+# quick BMI calclulator 
 height = 1.8
 wight = 74.3
 
@@ -10,4 +16,4 @@ def calc(h1, w1):
     return w1/(h1**2)
 
 bmi = calc(height, wight)
-print(round(bmi))
+print(round(bmi, 2))
