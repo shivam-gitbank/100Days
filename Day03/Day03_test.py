c98e523ec911f401921f22d7fba26906b5172b05
Shivam Sharma <ss5030200@gmail.com>
2026-03-24 22:17:02 +0530

    inital day 3 practice questions 2

--- Day03/Day03_test.py
+++ Day03/Day03_test.py
@@ -4,7 +4,7 @@ print("Welcome to the roller coaster !! ")
 height = float(input("What is your height in cm ? \n"))
 
 # conditional to check for the minimum required height
-if height < 120 :
+if height < 120:
     print("Sorry your height does not qualify please visit again when you are taller !\n")
 else:
     print("enjoy your ride :)")
