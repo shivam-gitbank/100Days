1b51b9d35f4ab0d31c9776fa963cd20384e8f3c4
Shivam Sharma <ss5030200@gmail.com>
2026-03-24 22:44:08 +0530

    coding exersie 2 odd even

--- Day03/Day03_test.py
+++ Day03/Day03_test.py
@@ -1,10 +1,18 @@
-# ticket counter code 
+# # ticket counter code challenge 1 
 
-print("Welcome to the roller coaster !! ")
-height = float(input("What is your height in cm ? \n"))
+# print("Welcome to the roller coaster !! ")
+# height = float(input("What is your height in cm ? \n"))
 
-# conditional to check for the minimum required height
-if height < 120:
-    print("Sorry your height does not qualify please visit again when you are taller !\n")
-else:
-    print("enjoy your ride :)")
+# # conditional to check for the minimum required height
+# if height < 120:
+#     print("Sorry your height does not qualify please visit again when you are taller !\n")
+# else:
+#     print("enjoy your ride :)")
+
+# coding challenge 2 ODD even check
+
+num = int(input("enter your desired number : "))
+if num % 2 == 0:
+    print("the number is even")
+else:    
+    print("number is odd")
