aa4c873b7640f6c513f06ae6a9d9ab3bfd3f9fcf
Shivam Sharma <ss5030200@gmail.com>
2026-03-24 23:04:06 +0530

    coding exersie 2 odd even edit 4

--- Day03/Day03_test.py
+++ Day03/Day03_test.py
@@ -8,10 +8,11 @@ if height < 120:
     print("Sorry your height does not qualify please visit again when you are taller !\n")
 else:
     age = int(input("what is your age ? \n"))
+# age check 
     if age <= 18:
-        print("The tickets for below 18 are $7.00 ")
+        print("please pay $7.00 ")
     elif age > 18:
-        print("The ticket price for above 18 is $12.00  ")
+        print("please pay $12.00  ")
     print("enjoy your ride :)\n")
 
 
