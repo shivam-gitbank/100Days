5e114edc6033ccc7055962b76501a5815e82fe5a
Shivam Sharma <ss5030200@gmail.com>
2026-03-25 19:00:54 +0530

    pizza project pilot - edits 1

--- Day03/pizza.py
+++ Day03/pizza.py
@@ -1,9 +1,11 @@
+# day 3 pizza project 
 print("Welcome to Python Pizza Deliveries !")
 size = input("What size of Pizza do you want? S, M or L : ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want Extra Cheese on your pizza> Y or N: ")
 total_bill = 0
 
+# pilot pizza project (conditional formatting)
 if size == 'S':
     total_bill = 5
     if pepperoni == 'Y':
@@ -14,7 +16,7 @@ if size == 'S':
             total_bill +=2
         elif extra_cheese == 'N':
             total_bill == total_bill
-    print(f"price for small pizza is ${total_bill}")
+    print(f"price for small pizza is ${total_bill}") 
 elif size == 'M':
     total_bill = 7
     if pepperoni == 'Y':
