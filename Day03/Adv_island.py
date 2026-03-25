3a97fc123d9ba131b8c5aa7eeb2a9aa7350e0a97
Shivam Sharma <ss5030200@gmail.com>
2026-03-26 00:51:57 +0530

    adv island tweaks final aesthetic

--- Day03/Adv_island.py
+++ Day03/Adv_island.py
@@ -2,11 +2,18 @@
 print("Welcome to Treasure Island. \nYour mission is to find the treasure! ")
 
 #conditionals 
-direction = input("You are at a cross road. Where do you want to go? Type \"Left\" or \"Right\" ? \n")
+direction = input("You are at a cross road. "
+                  "Where do you want to go? Type \"Left\" or \"Right\" ? \n")
+
 if direction.lower() == "left":
-    action = input("you come to a lake. There is an island in the middle of the lake. Type \"wait\" to wait for the boat or Type \"swim\" to swim accross \n")
+    action = input("you come to a lake. "
+                   "There is an island in the middle of the lake. "
+                   "Type \"wait\" to wait for the boat or Type \"swim\" to swim accross \n")
+    
     if action.lower() == "wait":
-        colour = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow, one blue. Which colour do you choose? \n")
+        colour = input("You arrive at the island unharmed. "
+                       "There is a house with 3 doors. One red, one yellow, one blue."
+                        " Which colour do you choose? \n")
         if colour.lower() == "red":
             print("It's a room full of fire. Game Over.... :_(")
         elif colour.lower() == "blue":
