07112fc0e49adf361ada9afceb083d9293ac69d3
Shivam Sharma <ss5030200@gmail.com>
2026-03-29 20:02:26 +0530

    Go Rand2

--- Day04/practice04.py
+++ Day04/practice04.py
@@ -1,6 +1,7 @@
 # Random library pactice - Day 04
 
 import random
-num = random.randint(1,5)
-ran_num = random.random() * 10
+num = random.randint(1,5) # choose a number inclusive of the range given - 1, 5 included
+ran_num = random.random() * 10 # generates a number between 0.0 and 1.0 - the result cannot be equal to 1.0 its always less than that 
+ran_flo = random.uniform(1, 10) # generates random float numbers inclusive of the range (a, b) included
 print(f"num is {num} and ran_int = {ran_num}") 
