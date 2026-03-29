1e0b14f9621a162438932e61f73c905d0e428a39
Shivam Sharma <ss5030200@gmail.com>
2026-03-29 20:09:11 +0530

    Coin Toss2

--- Day04/coin_toss.py
+++ Day04/coin_toss.py
@@ -1,7 +1,9 @@
+# random fun - small program coin toss by using random 
+
 import random
 coin = random.randint(0, 1)
 
 if coin == 0:
     print(" Heads ")
-elif coin == 1:
+else:
     print(" Tails ")
