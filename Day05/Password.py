ab73625f6ae6acf30e613e2e7e154399a46ec6de
Shivam Sharma <ss5030200@gmail.com>
2026-04-01 21:21:02 +0530

    correction

--- Day05/Password.py
+++ Day05/Password.py
@@ -5,20 +5,15 @@ letters = int(input("How many Letters would you like in your password?\n"))
 symbols = int(input("How many Symbols do you want in your password?\n"))
 numbers = int(input("How many numbers would you like in your password?\n"))
 
+pas_len = letters + symbols + numbers
+
 Symbol_list = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '{', '}', '[', ']', '<', '>', '?']
 letter_list = ['a','b','c','d','e','f','g','h', 'i','j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 numbers_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+password = ''
 
-for n in range(letters):
-    let = random.choice(letter_list)
-    password += let
-    for i in range(symbols):
-        sym = random.choice(Symbol_list)
-        password += sym
-        for k in range(numbers):
-            numb = random.choice(numbers_list)
-            password += numb
+for i in range(pas_len):
+    s1 = random.choice(Symbol_list)
+    password += s1
 
-print(f"your password is {password}")
-
-
+print(f"{password}")
