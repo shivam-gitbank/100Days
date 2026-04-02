42756e596e502fdfdb6381b59a8c17de3847f00e
Shivam Sharma <ss5030200@gmail.com>
2026-04-02 20:05:02 +0530

    aesthetic fixes

--- Day05/Password.py
+++ Day05/Password.py
@@ -9,7 +9,7 @@ numbers = int(input("How many numbers would you like in your password?\n"))
 
 # List for every element required to create password
 Symbol_list = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '{', '}', '[', ']', '<', '>', '?']
-letter_list = ['a','b','c','d','e','f','g','h', 'i','j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+letter_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i','j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
                'A', 'B', 'C', 'D', 'E','F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 password = []
