07e770c87c6c4ed44224c9129adbf1f58bb31949
Shivam Sharma <ss5030200@gmail.com>
2026-04-05 01:20:56 +0530

    fased out

--- Day05/practice_day05.py
+++ Day05/practice_day05.py
@@ -1,3 +1,12 @@
 fruits = ["Apple", "Peach", "Pear"]
 for fruit in fruits:
     print(fruit)
+
+student_scores = [45,34,63,43,6,67,5,34345,45,45,34,345,345,34,3,45345,345,34,5,345,34,543,5345]
+max = 0 
+for i in student_scores:
+    if max < i:
+        max = i
+
+print(max)
+        
