39cab7b7dc87e9a57296f0eda1ea41353af0d517
Shivam Sharma <ss5030200@gmail.com>
2026-05-03 23:51:28 +0530

    inital push

--- Day06/functions_day06.py
+++ Day06/functions_day06.py
@@ -1,1 +1,21 @@
-print("func")
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif wall_in_front():
+        turn_left()
+        
+
+while at_goal() != True:
+    if wall_in_front():
+        jump()
+    elif right_is_clear():
+        turn_right()
+        move()
+    else:
+        move()
