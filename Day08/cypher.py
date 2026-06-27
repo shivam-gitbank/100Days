ba32af00db5bf0538cf6aa78a742c5185c2f5ded
Shivam Sharma <ss5030200@gmail.com>
2026-06-28 00:10:40 +0530

    Cypher logic ready

--- Day08/cypher.py
+++ Day08/cypher.py
@@ -1,16 +1,15 @@
 # encoding logic - encrytption of text 
 def encode():
-    base_text = input("Type your message\n").lower()
+    base_text = input("Type your message\n")
     shift_number = int(input("Type the shift number\n"))
     cypher_text = ""
 
 # Loop for processing each letter in cypher 
     for e in base_text:
         t = chr(ord(e) + shift_number)
-        if t >= chr(97) and t <= chr(122):
+        if t >= chr(65) and t <= chr(90):
             cypher_text += t
-        # upper case intact
-        elif t >= chr(65) and t <= chr(90):
+        elif t >= chr(97) and t <= chr(122):
             cypher_text += t
         # Z letter edge case 
         else:
@@ -18,3 +17,4 @@ def encode():
     return cypher_text    
 
 cy = encode()
+print(cy)
