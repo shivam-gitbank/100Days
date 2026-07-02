054bab1e6533f0d8b109febe64baa9a489697151
Shivam Sharma <ss5030200@gmail.com>
2026-07-03 00:28:18 +0530

    not sure what

---
+++ Day08/extras_day08.py
@@ -0,0 +1,9 @@
+def life_in_weeks(age):
+    max_life = 90
+    years_left = max_life - age
+    week_left = years_left * 52 # as average weeks are 52
+    return week_left
+    
+
+age = int(input("Hi whats your age "))
+print(f"life left in weeks {life_in_weeks(age)}")
