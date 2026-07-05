48513103f88b0e4e89dc4f43838ee3722077ff6a
Shivam Sharma <ss5030200@gmail.com>
2026-07-05 11:42:22 +0530

    love cal functional now working as expected

--- Day08/extras_luvcal.py
+++ Day08/extras_luvcal.py
@@ -1,15 +1,30 @@
 def love_calculator(name1, name2):
+     
     # True Function 
      def true(n1, n2):
           total_true = 0
           namelist = [n1, n2]
           for name in namelist:
                for i in name:
-                    if i in "True":
+                    if i in "true":
                          total_true += 1
           return total_true
-     true_total1 = true(name1, name2)
-     return true_total1
+     
+     #Love Functuion
+     def love(n1, n2):
+          total_love = 0
+          name_list = [n1, n2]
+          for name in name_list:
+               for i in name:
+                    if i in "love":
+                         total_love += 1
+          return total_love
+     
+     love_total = love(name1, name2)
+     true_total = true(name1, name2)
+     final_total = int((true_total * 10) + love_total)
+     return final_total
 
-true = int(love_calculator("Angela Yu", "Jack Brauer")) * 10
-print(true)
+
+love_cal = love_calculator("Angela Yu", "Jack Bauer")
+print(f"total % = {love_cal}")
