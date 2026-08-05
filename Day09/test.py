01ad07dd55ead6ac6730fc9bed2a6cbc04c1d3ea
shivam-gitbank <ss5030200@gmail.com>
2026-08-05 19:05:40 +0530

    test for dic access\]

---
+++ Day09/test.py
@@ -0,0 +1,7 @@
+travel_log = {
+    "France": ['paris', 'Lille', 'Dujon'],
+    "Germany": ['frankfurt', 'stuttgart', 'Berlin'],
+}
+
+print(travel_log['France'][1]) # as tavel_log[France] is itself a list i can index it directly 
+# think of it as frace named list - index 1 value is -Lillie
