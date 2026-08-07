af42b5c001432fddf59d9db07a151b7266e777ea
shivam-gitbank <ss5030200@gmail.com>
2026-08-08 00:04:28 +0530

    function completed testing functionality

--- Day10/calc.py
+++ Day10/calc.py
@@ -16,20 +16,20 @@ def calculator(val1, opr, val2):
 # While loop condition 
 repeat = True
 while repeat == True: 
-    value = int(input("Please enter your value"))
-    operator = input("Please specify the operation  '+', '-', '*', '/' '%' ") # operator
-    val2 = int(input("Enter the value for operation"))
+    value = int(input("Please enter your value "))
+    operator = input("Please specify the operation  '+', '-', '*', '/' '%'  ") # operator
+    val2 = int(input("Enter the value for operation "))
     final_val = calculator(value, operator, val2)
     print(f"the output is {value} {operator} {val2} = {final_val}")
 
 # continuation logic for previous result set
-    conti = input("do you wish to continue with the same result")
+    conti = input("do you wish to continue with the same result ")
     if conti == 'yes':
-        opr2 = input("specify operator")
-        val3 = int(input("enter value to continue"))
+        opr2 = input("specify operator ")
+        val3 = int(input("enter value to continue "))
         final_val2 = calculator(final_val, opr2, val3)
-        repeat = True
-        print(f"the output is {final_val} {operator} {val3} = {final_val2}")
+        print(f"the output is {final_val} {opr2} {val3} = {final_val2} ")
+        continue
     else:
         print(f"Power off, good bye!  :)")
         repeat = False
