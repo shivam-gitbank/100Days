2a6e0119c4f89895c75e213fa119d74ce981278f
shivam-gitbank <ss5030200@gmail.com>
2026-08-07 23:53:49 +0530

    bulding the while loop

--- Day10/calc.py
+++ Day10/calc.py
@@ -19,4 +19,15 @@ while repeat == True:
     operator = input("Please specify the operation  '+', '-', '*', '/' '%' ")
     val2 = int(input("Enter the value for operation"))
     final_val = calculator(value, operator, val2)
+    conti = input("do you wish to continue with the same result")
+    print(f"")
+    if conti == 'yes':
+        opr2 = input("specify operator")
+        val3 = int(input("enter value to continue"))
+        final_val = calculator(final_val, opr2, val3)
+        repeat = True
+        print(f"")
+    else:
+        print(f"")
+        repeat = False
     
