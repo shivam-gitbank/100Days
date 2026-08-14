ad37386c7be8aa6b2c7b4940d6d9b74a2b65bc03
shivam-gitbank <ss5030200@gmail.com>
2026-08-14 23:26:07 +0530

    commit for toutor code

---
+++ Day10/calc_dic.py
@@ -0,0 +1,35 @@
+def add (n1, n2):
+    return (n1+n2)
+
+def subtract (n1, n2):
+    return n1-n2
+
+def divide(n1, n2):
+    return n1/n2
+
+def multiply (n1, n2):
+    return n1*n2
+
+operation = {
+'+' : add,
+'-' : subtract,
+'*' : multiply,
+'/' : divide,
+}
+
+val1 = int(input("enter your value "))
+aggregate = True
+while aggregate == True:
+    operator = input("choose your operator ")
+    val2 = int(input("enter second value"))
+    result = operation[operator](val1, val2)
+    cont = input("to continue with result press 'y' to start new cal press 'n' to exit press'E'")
+    if cont == 'y':
+        val1 = result
+        continue
+    elif cont == 'n':
+        val1 = int(input("enter value"))
+        continue
+    else:
+        aggregate = False
+        print("thanks come again :)")
