64cfeee25977edfaf17d08b2c334c11bf89b3fb5
shivam-gitbank <ss5030200@gmail.com>
2026-08-15 20:58:46 +0530

    additon logic

--- Day11/Blackjack.py
+++ Day11/Blackjack.py
@@ -1,5 +1,6 @@
 import random
 
+# J, Q, K are considered as 10 for blackjack
 cards = {
     "Spades": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'], 
     "Clubs": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'],
@@ -29,13 +30,13 @@ def draw(my_list, comp_list):
     comp_sum = 0
     draws = True
     while draws == True:
-        if my_cards.__add__() < 21:
-            my_sum = my_cards.__add__()
+        if my_list.__add__() < 21 or comp_list.__add__() < 21:
+            my_sum = my_list.__add__()
             comp_sum = comp_list.__add__()
             print(my_sum, comp_sum)
 
 
-
+draw(my_list, comp_list)
         
 
 
