d96cbe7f7d0ba1b18939a5d0f6c1fcdace4130a2
shivam-gitbank <ss5030200@gmail.com>
2026-08-15 22:02:04 +0530

    further into programming logic for blackjack J, K , Q logic

--- Day11/Blackjack.py
+++ Day11/Blackjack.py
@@ -8,34 +8,47 @@ cards = {
     "Hearts": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'],
 }
 
-clubs = ["Spades", "Clubs", "Diamonds", "Hearts"]
+clubs = ["Spades", "Clubs", "Diamonds", "Hearts"] # list of keys to access card numbers, another way would  have been 2 list
 
 def my_cards(card):
     my = []
     for i in range(2):
-        my.append(random.choice(cards[random.choice(clubs)])) # random.choice(clubs)- access club list, outer random picks one value
+        chosen_card = random.choice(cards[random.choice(clubs)])# random.choice(clubs)- access club list, outer random picks one value
+        if chosen_card == 'J' or chosen_card == 'K' or chosen_card =='Q': # Black jack J, K, Q logic
+            my.append(10)
+        else:
+            my.append(chosen_card)
     return my
 
 def comp_cards(card):
     comp = []
     for i in range(2):
-        comp.append(random.choice(cards[random.choice(clubs)]))
+        chosen_card = random.choice(cards[random.choice(clubs)])# random.choice(clubs)- access club list, outer random picks one value
+        if chosen_card == 'J' or chosen_card == 'K' or chosen_card =='Q':
+            comp.append(10)
+        else:
+            comp.append(chosen_card)
     return comp
+
+def draw_more()
             
 my_list = my_cards(cards)
 comp_list = comp_cards(cards)
 
 def draw(my_list, comp_list):
-    my_sum = 0
-    comp_sum = 0
+    my_sum = sum(my_list)
+    comp_sum = sum(comp_list)
     draws = True
     while draws == True:
-        if my_list.__add__() < 21 or comp_list.__add__() < 21:
-            my_sum = my_list.__add__()
-            comp_sum = comp_list.__add__()
-            print(my_sum, comp_sum)
-
-
+        if my_sum < 21 and comp_sum > 21:
+            draws = False
+            print("YOU WIN $$$ !!! :)")
+        elif my_sum > 21 and comp_sum < 21:
+            draws = False
+            print("YOU LOSE :*( ")
+        elif my_sum < 21 and comp_sum < 21:
+            draws = True
+            my_sum1, comp_sum1 = draw_more(my_sum, comp_sum)
 draw(my_list, comp_list)
         
 
