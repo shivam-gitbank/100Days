bf4af1a053d6aa0f23d056263edb9b80e576ceea
shivam-gitbank <ss5030200@gmail.com>
2026-08-15 20:53:11 +0530

    Black jack logic placement

--- Day11/Blackjack.py
+++ Day11/Blackjack.py
@@ -12,7 +12,31 @@ clubs = ["Spades", "Clubs", "Diamonds", "Hearts"]
 def my_cards(card):
     my = []
     for i in range(2):
-        my.append(random.choice(cards[random.choice(clubs)]))
-    print(my)
+        my.append(random.choice(cards[random.choice(clubs)])) # random.choice(clubs)- access club list, outer random picks one value
+    return my
 
-my_cards(cards)
+def comp_cards(card):
+    comp = []
+    for i in range(2):
+        comp.append(random.choice(cards[random.choice(clubs)]))
+    return comp
+            
+my_list = my_cards(cards)
+comp_list = comp_cards(cards)
+
+def draw(my_list, comp_list):
+    my_sum = 0
+    comp_sum = 0
+    draws = True
+    while draws == True:
+        if my_cards.__add__() < 21:
+            my_sum = my_cards.__add__()
+            comp_sum = comp_list.__add__()
+            print(my_sum, comp_sum)
+
+
+
+        
+
+
+
