d21e072005b54ff61f979498058c1edb38a001dd
shivam-gitbank <ss5030200@gmail.com>
2026-08-24 18:00:30 +0530

    recusion solution: git push

--- Day11/Blackjack.py
+++ Day11/Blackjack.py
@@ -26,22 +26,38 @@ for i in range(2): # choosing random 2 cards for both player and comp
 def draw(my_list, comp_list):
     my_sum = sum(my_list)
     comp_sum = sum(comp_list)
-    draws = True
-    while draws == True:
-        if my_sum < 21 and comp_sum > 21:
-            draws = False
-            print(f"your cards sum {my_sum}, opp card sum {comp_sum}")
-            print(f"YOU WIN $$$ !!! :)")
-        elif my_sum > 21 and comp_sum < 21:
-            draws = False
-            print(f"{my_sum}, {comp_sum}")
-            print(f"YOU LOSE :*( ")
-        elif my_sum < 21 and comp_sum < 21:
-            draws = True
-            print(f"your sum {my_sum}, opp sum {comp_sum}")
-            choice = input("do you wish to draw more press 'yes' else 'No'").lower()
-            if choice == "yes":
-                random_cards
+    print(f"your current card total = {my_sum} \nopp current cards sum = {comp_sum}")
+    choice = input("do you wish to draw more cards ? Yes - y or No - n ").lower
+    if choice == 'y' and my_sum < 21:
+        my_list.append(random_cards)
+        comp_list.append(random_cards)
+        my_sum = sum(my_list)
+        comp_sum = sum(comp_list)
+        if my_sum == 21:
+            print("you win {my_sum}")
+    else:
+        if my_sum < 21 and my_sum > comp_sum:
+            print(f"you won as you are closer to 21 than your opp!")
+        elif my_sum > 21 and comp_sum <= 21:
+            print(f"you went over 21 you lose!")
+        elif my_sum == comp_sum:
+            print("its a draw ")
+    
+
+            
+        #     draws = False
+        #     print(f"your cards sum {my_sum}, opp card sum {comp_sum}")
+        #     print(f"YOU WIN $$$ !!! :)")
+        # elif my_sum > 21 and comp_sum < 21:
+        #     draws = False
+        #     print(f"{my_sum}, {comp_sum}")
+        #     print(f"YOU LOSE :*( ")
+        # elif my_sum < 21 and comp_sum < 21:
+        #     draws = True
+        #     print(f"your sum {my_sum}, opp sum {comp_sum}")
+        #     choice = input("do you wish to draw more press 'yes' else 'No'").lower()
+        #     if choice == "yes":
+        #         random_cards
              
 
         
